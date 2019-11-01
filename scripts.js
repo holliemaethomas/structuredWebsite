d266643a586df8a45960bb2e31bb7f9d6cef00b7
holliemaethomas <holliemaethomas85@gmail.com>
2019-11-01 11:15:16 -0700

    pushing to update with js functions

---
+++ scripts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var greeting = alert('Hello there and welcome');
+
+var userName = prompt('What is your name?');
+
+console.log('the user entered ' + userName);
+
+var returnName = alert('Ok ' + userName + ' lets take a look at a basic html layout');
