9c2ce27ce3c1ca14106dabe1935c3123d64c0c69
holliemaethomas <holliemaethomas85@gmail.com>
2019-11-02 19:48:03 -0700

    added to uppercase function

--- scripts.js
+++ scripts.js
@@ -4,6 +4,6 @@ var greeting = alert('Hello there and welcome');
 
 var userName = prompt('What is your name?');
 
-console.log('the user entered ' + userName);
+console.log('the user entered ' + userName.toUpperCase());
 
-var returnName = alert('Ok ' + userName + ' lets take a look at a basic html layout');
+var returnName = alert('Ok ' + userName.toUpperCase() + ' lets take a look at a basic html layout');
